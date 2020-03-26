6121871a0bb81deb247abbeb96bf959c7023c5a5
scl <1558053958@qq.com>
2020-03-26 13:14:24 +0800

    删除charts

--- lib/view/line/day.dart
+++ lib/view/line/day.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:smile/charts/charts.dart';
+import 'package:flutter_charts/flutter_charts.dart';
 import 'package:smile/config/nets/api.dart';
 import 'package:smile/config/constant.dart';
 import 'package:smile/models/chart.dart';
